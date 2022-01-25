4ab92ebefc1a15b8652147d1fbb33d0b31b52835
antonio-cajueiro-campos <kimeesan5@gmail.com>
2022-01-25 18:58:48 -0300

    1st proof of concept

---
+++ src/main/java/com/greenprojects/oilrescue/OilrescueApplication.java
@@ -0,0 +1,13 @@
+package com.greenprojects.oilrescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilrescueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilrescueApplication.class, args);
+	}
+
+}
